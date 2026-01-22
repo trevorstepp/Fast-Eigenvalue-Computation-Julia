72ba473febc501e5b2f82dd60424c5503b9e03b8
Trevor Stepp <181792165+trevorstepp@users.noreply.github.com>
2026-01-21 19:59:09 -0500

    plotting

--- plot_results.jl
+++ plot_results.jl
@@ -1,4 +1,29 @@
-using Serialization
+using CSV, DataFrames
 using Plots
 
 # load saved data
+df = CSV.read("timings.csv", DataFrame)
+
+# create plot with block algorithm times 
+plot(
+    df.n,
+    df.block_time,
+    yscale = :log10,
+    marker = :circle,
+    label = "Our approach (all)",
+    xlabel = "discretization size (n)",
+    ylabel = "run time (s)",
+    title = "All eigenpairs runtime (K=3)",
+    grid = true
+)
+
+# add dense algorithm times
+plot!(
+    df.n,
+    df.dense_time,
+    marker = :square,
+    label = "Dense eig (full)"
+)
+
+savefig("K=3.png")
+display(current())
